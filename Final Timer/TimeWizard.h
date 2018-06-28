5ba795349813b5d22456c7213c94d9f546ca2e21
SAMAEL NEWGATE <SAMNEWGA@uat.edu>
2018-06-27 23:37:54 -0700

    Done

--- Final Timer/TimeWizard.h
+++ Final Timer/TimeWizard.h
@@ -2,23 +2,29 @@
 #include "stdafx.h"
 #include <iostream>
 
+//using namespace standard
 using namespace std;
 
+//Declares a class named Wizard and its function
 class TimeWizard{
+//Everything here is public
 public:
 
 	void timeIt();
 
+//Everything here is private
 private:
 
 };
 
+//Declares a class named DeathSpawn and its function
 class DeathSpawn{
+//Everything here is public
 public:
 	void talk(){
 		cout << "\nDeath: It's been 15 minutes, I'm here to take your life!" << endl;
 	}
-
+//Everything here is private
 private:
 
 };
